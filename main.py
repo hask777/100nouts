452636c66ae92e777f7ac6a669831d6387a2f8fc
hask777 <haskellisp@gmail.com>
2023-01-05 12:31:18 +0300

    f

--- main.py
+++ main.py
@@ -2,37 +2,78 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+def get_books():
 
-response = requests.get('https://100nout.by/product-category/kompjuternaja-tehnika/noutbuki/?proizvoditel=dell,lenovo&tip=biznes&razr=fhd-1920x1080&tech=ips',
-headers=headers).text
+    url = 'https://100nout.by/product-category/kompjuternaja-tehnika/noutbuki/?proizvoditel=lenovo'
 
-soup = BeautifulSoup(response, 'lxml')
+    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
 
-arr = []
+    response = requests.get(url,
+    headers=headers).text
 
-items = soup.find_all('li', class_='catalog__list-item')
+    soup = BeautifulSoup(response, 'lxml')
 
-for item in items:
-    link = item.find('a', class_='p-card__pic').get('href')
-    title = item.find('a', class_='p-card__info-more-title').text.strip()
-    info = item.find('div', class_='p-card__info-more-short-desc')
-    for p in info:
-        pass
-         
-        
-    sale_price = item.find('div', class_='salep sale__price').text
-    price_value = item.find('span', class_='p-card__price-value').text
-    # print(sale_price)
+    pages = soup.find('ul', class_='page-numbers')
+    pages = pages.find_all('li')
+    last_page = pages[-2].text
 
-    book = dict(
-        link = link,
-        title = title,
-        # param = param,
-        sale_price = sale_price,
-        price_value = price_value,
-    )
-    arr.append(book)
+    arr = []
+    count = 0
+
+    for page in range(1, int(last_page)+1):
+    
+        url = f'https://100nout.by/product-category/kompjuternaja-tehnika/noutbuki/page/{page}/?proizvoditel=lenovo'
+
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+        response = requests.get(url,
+        headers=headers).text
+        # print(url)
+
+        soup = BeautifulSoup(response, 'lxml')
+        items = soup.find_all('li', class_='catalog__list-item')
+
+        for item in items:
+            link = item.find('a', class_='p-card__pic').get('href')
+            title = item.find('a', class_='p-card__info-more-title').text.replace('\n', '').strip()
+            info = item.find('div', class_='p-card__info-more-short-desc').find_all('p')
+            
+            params = []
+            # print(info)
+            for s in info:
+                s = s.find_all('span')
+                for i in s:
+                    name = s[0].text
+                    value = s[1].text
+                        
+                params.append( dict(
+                    name = name.strip(),
+                    value = value.strip()
+                ))       
+
+            sale_price = item.find('div', class_='salep sale__price').text
+            price_value = item.find('span', class_='p-card__price-value').text
+            count += 1
+
+            book = dict(
+                link = link,
+                title = title,
+                params = params,
+                sale_price = sale_price,
+                price_value = price_value,
+                count = count
+            )
+
+            arr.append(book)
+            # print(len(arr))
+            
+        arr.append(dict(total = count))
 
     with open('books.json', 'w', encoding='utf-8')  as f:
         json.dump(arr, f, ensure_ascii=False, indent=4)
+
+def main():
+    get_books()
+
+
+if __name__ == '__main__':
+    main()
